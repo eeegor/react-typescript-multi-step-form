43ed1d32a90b99f208a1297dcb2ab351725caf84
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-01 15:52:50 +0100

    draft: App component displays a data-driven FormMultiStep form with tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,53 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './App';
+import { mount } from 'enzyme';
 
 describe('App', () => {
 	it('renders without crashing', () => {
 		const div = document.createElement('div');
 		ReactDOM.render(<App />, div);
 		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	describe('Form Text Fields', () => {
+		const testData = [
+			{ type: 'text', name: 'name', value: 'Demo' },
+			{ type: 'text', name: 'email', value: 'info@example.com' },
+			{ type: 'text', name: 'phone', value: '+49 160 3232323' },
+			{
+				type: 'select-radio',
+				name: 'salary',
+				value: '1.000 - 2.000',
+			},
+		];
+
+		testData.forEach((testSet, index) => {
+			it(`can change and submit ${testSet.name} input`, () => {
+				const nextValue = { target: { value: testSet.value } };
+				const wrapper = mount(<App />);
+				const formMultiStep = wrapper.find('#form-multi-step--subscribe');
+				const formStep = formMultiStep.find(`#form-step-${index + 1}`);
+				switch (testSet.type) {
+					case 'select-radio':
+						const formRadioGroup = formStep.find('.input-select--radio');
+						const formSelectRadioInput = formRadioGroup.find('.input-radio').first();
+						formSelectRadioInput.simulate('focus');
+						formSelectRadioInput.simulate('change', nextValue);
+						formRadioGroup.simulate('submit', nextValue);
+						break;
+
+					case 'text':
+					default:
+						const formTextGroup = formStep.find('.input-text');
+						formTextGroup.simulate('focus');
+						formTextGroup.simulate('change', nextValue);
+						formTextGroup.simulate('submit', nextValue);
+						break;
+				}
+				const formState = wrapper.state()['form'];
+				expect(formState[testSet.name]).toEqual(testSet.value);
+			});
+		});
 	});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,51 +5,60 @@
 import * as React from 'react';
 import './index.scss';
 import './App.scss';
+import { FormMultiStep, FormSchema } from './components/FormMultiStep';
+import { Props as InputGroupTextProps } from './components/InputGroupText';
+import { Props as InputSelectRadioProps } from './components/InputSelectRadio';
+
+const formMultiStepSchema: { [key: number]: InputGroupTextProps | InputSelectRadioProps } = {
+	1: { type: 'text', id: 'name', name: 'name', label: 'Your full name', value: 'Demo' },
+	2: {
+		type: 'email',
+		id: 'email',
+		name: 'email',
+		label: 'Your email',
+		value: 'info@example.com',
+	},
+	3: { type: 'text', id: 'phone', name: 'phone', label: 'Your phone', value: '+49 160 3232323' },
+	4: {
+		type: 'select-radio',
+		id: 'salary',
+		name: 'salary',
+		label: 'Select you salary',
+		values: [
+			'€ 0 - € 1.000',
+			'€ 1.000 - € 2.000',
+			'€ 2.000 - € 3.000',
+			'€ 3.000 - € 4.000',
+			'More then € 4.000',
+		],
+	},
+};
 
 interface Props {}
+interface State {
+	form: FormSchema;
+}
 
-export class App extends React.Component<Props> {
+export class App extends React.Component<Props, State> {
+	handleFormChange = (form: FormSchema) => {
+		this.setState(
+			state => ({
+				...state,
+				form,
+			})
+		);
+	};
+
 	render(): JSX.Element {
 		return (
 			<div className="app">
 				<div className="content">
 					<div className="container">
-						<h1>Welcome</h1>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-							quaerat similique omnis. Quisquam consectetur minus fuga unde, sed
-							molestiae nesciunt reiciendis tenetur!
-						</p>
-						<h2>Welcome</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-							quaerat similique omnis. Quisquam consectetur minus fuga unde, sed
-							molestiae nesciunt reiciendis tenetur!
-						</p>
-						<h3>Welcome</h3>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-							quaerat similique omnis. Quisquam consectetur minus fuga unde, sed
-							molestiae nesciunt reiciendis tenetur!
-						</p>
-						<h4>Welcome</h4>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-							quaerat similique omnis. Quisquam consectetur minus fuga unde, sed
-							molestiae nesciunt reiciendis tenetur!
-						</p>
-						<h5>Welcome</h5>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-							quaerat similique omnis. Quisquam consectetur minus fuga unde, sed
-							molestiae nesciunt reiciendis tenetur!
-						</p>
-						<h6>Welcome</h6>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-							quaerat similique omnis. Quisquam consectetur minus fuga unde, sed
-							molestiae nesciunt reiciendis tenetur!
-						</p>
+						<FormMultiStep
+							id="subscribe"
+							formSchema={formMultiStepSchema}
+							onChange={formData => this.handleFormChange(formData)}
+						/>
 					</div>
 				</div>
 			</div>
