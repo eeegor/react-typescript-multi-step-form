2d331c5b200ce4b48c443854a5646b8002da6c2d
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-03 18:42:47 +0100

    update: FormProgress tests

---
+++ src/components/FormProgress/FormProgress.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import { FormProgress } from './FormProgress';
+
+describe('FormProgress', () => {
+	it('renders markup correctly', () => {
+		const wrapper = <FormProgress maxSteps={3} currentStep={1} />;
+		const tree = renderer.create(wrapper).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+});
---
+++ src/components/FormSuccess/FormSuccess.test.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import { FormSuccess } from './FormSuccess';
+import { mount } from 'enzyme';
+
+describe('FormSuccess', () => {
+	
+	const callback = jest.fn();
+	
+	it('renders markup correctly', () => {
+		const wrapper = <FormSuccess onReset={callback} />;
+		const tree = renderer.create(wrapper).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+	
+	it('click event works', () => {
+		const wrapper = mount(<FormSuccess onReset={callback} />);
+		wrapper.find('.form-success__reset').first().simulate('click');
+		expect(callback).toHaveBeenCalledTimes(1);
+	});
+});
