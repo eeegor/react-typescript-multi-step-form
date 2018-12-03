19b2c80ea6635eccec8ba05d77794113029d89b8
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-03 19:05:47 +0100

    update: InputControl tests

---
+++ src/components/InputControl/InputControl.test.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import { InputControl } from './InputControl';
+import { mount, shallow } from 'enzyme';
+
+describe('InputControl', () => {
+	it('renders markup correctly', () => {
+		const component = <InputControl id="a-unique-id" />;
+		const tree = renderer.create(component).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it('should adopt a status class from attribute', () => {
+		const wrapper = shallow(<InputControl id="a-unique-id" status="success" />);
+		expect(wrapper.hasClass('input-control--success')).toEqual(true);
+	});
+	
+	it('should adopt a valid class if valid', () => {
+		const wrapper = shallow(<InputControl id="a-unique-id" valid={true} />);
+		expect(wrapper.hasClass('input-control--success')).toEqual(true);
+	});
+	
+	it('should adopt a status danger class if has errors', () => {
+		const wrapper = shallow(<InputControl id="a-unique-id" errors={'Error'} />);
+		expect(wrapper.hasClass('input-control--danger')).toEqual(true);
+	});
+	
+	it('should adopt a required class from attribute', () => {
+		const wrapper = shallow(<InputControl id="a-unique-id" required={true} />);
+		expect(wrapper.hasClass('input-control--required')).toEqual(true);
+	});
+});
