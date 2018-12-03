c14113ed420c5123d09f4b229d033326da481f80
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-03 18:50:16 +0100

    update: FormSuccess tests

--- src/components/FormSuccess/FormSuccess.test.tsx
+++ src/components/FormSuccess/FormSuccess.test.tsx
@@ -4,18 +4,20 @@ import { FormSuccess } from './FormSuccess';
 import { mount } from 'enzyme';
 
 describe('FormSuccess', () => {
-	
 	const callback = jest.fn();
-	
+
 	it('renders markup correctly', () => {
 		const wrapper = <FormSuccess onReset={callback} />;
 		const tree = renderer.create(wrapper).toJSON();
 		expect(tree).toMatchSnapshot();
 	});
-	
+
 	it('click event works', () => {
 		const wrapper = mount(<FormSuccess onReset={callback} />);
-		wrapper.find('.form-success__reset').first().simulate('click');
+		wrapper
+			.find('.form-success__reset')
+			.first()
+			.simulate('click');
 		expect(callback).toHaveBeenCalledTimes(1);
 	});
 });
