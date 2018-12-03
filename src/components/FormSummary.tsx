0c6048fd2b392031d1b2e96302e1e0ed5e529b74
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-03 02:26:03 +0100

    add component: FormSummary

---
+++ src/components/FormSummary.tsx
@@ -0,0 +1,34 @@
+/**
+ * Component FormSummary
+ */
+
+import * as React from 'react';
+import * as classnames from 'classnames';
+import { Label } from './Label';
+import './FormSummary.scss';
+
+export interface Props {
+	className?: string;
+	formFields: object;
+	label?: string | JSX.Element;
+}
+
+export class FormSummary extends React.Component<Props> {
+	render(): JSX.Element {
+		const { className, label, formFields } = this.props;
+
+		return (
+			<div className={classnames('form-summary', className)}>
+				{label && <Label className="form-summary__label" label={label} />}
+				<div className="table">
+					{Object.keys(formFields).map(formField => (
+						<div className="table__row" key={formField}>
+							<div className="table__row__label">{formField}</div>
+							<div className="table__row__value">{formFields[formField]}</div>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	}
+}
