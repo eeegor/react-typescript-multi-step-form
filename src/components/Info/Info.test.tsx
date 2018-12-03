3306264ebcad58cfb9d3574f90ab474219e294ba
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-03 18:56:51 +0100

    update: Info tests

---
+++ src/components/Info/Info.test.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import { Info } from './Info';
+import { mount } from 'enzyme';
+
+describe('Info', () => {
+	it('renders markup correctly', () => {
+		const wrapper = <Info label="hello" />;
+		const tree = renderer.create(wrapper).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it('should not output a label with empty value', () => {
+		const wrapper = mount(<Info label="" />);
+		expect(wrapper.find('.label')).toHaveLength(0);
+	});
+});
