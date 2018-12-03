f5bdee5f2600ad965b90da1252f23682dc9d660b
Egor Kirpichev <egor.kirpichev@gmail.com>
2018-12-03 10:31:09 +0100

    fix: IconSuccess svg

--- src/components/IconSuccess/IconSuccess.tsx
+++ src/components/IconSuccess/IconSuccess.tsx
@@ -18,19 +18,24 @@ export class IconSuccess extends React.Component<Props> {
 
 		return (
 			<div className={classnames('icon-success', className)}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="160px" height="160px" viewBox="0 0 72 72">
-					<g fill="none" stroke="#8EC343" strokeWidth="2">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="160px"
+					height="160px"
+					viewBox="0 0 72 72"
+				>
+					<g fill="none" stroke="#8EC343" strokeWidth="3">
 						<circle
 							className="icon-success__circle"
 							cx="36"
 							cy="36"
-							r="35"
-							style={{strokeDasharray: '240px, 240px', strokeDashoffset: '480px'}}
+							r="34"
+							style={{ strokeDasharray: '240px, 240px', strokeDashoffset: '480px' }}
 						/>
 						<path
 							className="icon-success__path"
 							d="M17.417,37.778l9.93,9.909l25.444-25.393"
-							style={{strokeDasharray: '50px, 50px', strokeDashoffset: '0px'}}
+							style={{ strokeDasharray: '50px, 50px', strokeDashoffset: '0px' }}
 						/>
 					</g>
 				</svg>
